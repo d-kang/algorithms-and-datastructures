102dd13b6a4e98f5c7819ba66818cfc75a40761d
d-kang <david.kang714@gmail.com>
2017-12-02 20:42:13 -0800

    Init MergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -2,5 +2,16 @@
 * @Author: David Kang
 * @Date:   12.01.2017 06:13pm
 * @Filename: mergeSort.js
-* @Last modified time: 12.01.2017 06:13pm
+* @Last modified time: 12.02.2017 08:42pm
 */
+
+
+const mergeSort = function(nums) {
+  const midpoint = nums.length / 2;
+
+  if (nums.length === 1) {
+    return;
+  }
+
+  return nums;
+}
