752710548148370fe96b5d75aac41b538c3457d8
d-kang <david.kang714@gmail.com>
2017-12-08 11:57:07 -0800

    Solve for Base Case 4 Using TDD

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -25,14 +25,25 @@ const towersOfHanoi = (towers) => {
     moveDisc(start, end);
     moveDisc(temp, end);
   }
+  if (start.length === 3) {
+    moveDisc(start, end);
+    moveDisc(start, temp);
+    moveDisc(end, temp);
+    moveDisc(start, end);
+    /* 2 discs, rotate start temp end and recurse? */
+    moveDisc(temp, start);
+    moveDisc(temp, end);
+    moveDisc(start, end);
+  }
   return towers;
 }
 
 
 
-(function() {
+(function(numberOfDiscs) {
   console.log('-------------------------------')
   console.log('-------------------------------')
+  console.log('numberOfDiscs', numberOfDiscs);
   const input = [[],[],[]];
   const expected = [[], [], []];
   const actual = towersOfHanoi(input);
@@ -42,11 +53,12 @@ const towersOfHanoi = (towers) => {
   console.log(`${sInput}>>>`, sExpected === sActual)
   console.log('actual>>>', actual);
   console.log('expected>>>', expected);
-})();
+})(0);
 
 
-(function() {
+(function(numberOfDiscs) {
   console.log('-------------------------------')
+  console.log('numberOfDiscs', numberOfDiscs);
   const input = [[1],[],[]];
   const expected = [[], [], [1]];
   const actual = towersOfHanoi(input);
@@ -56,11 +68,12 @@ const towersOfHanoi = (towers) => {
   console.log(`${sInput}>>>`, sExpected === sActual)
   console.log('actual>>>', actual);
   console.log('expected>>>', expected);
-})();
+})(1);
 
 
-(function() {
+(function(numberOfDiscs) {
   console.log('-------------------------------')
+  console.log('numberOfDiscs', numberOfDiscs);
   const input = [[2, 1],[],[]];
   const expected = [[], [], [2, 1]];
   const actual = towersOfHanoi(input);
@@ -70,4 +83,19 @@ const towersOfHanoi = (towers) => {
   console.log(`${sInput}>>>`, sExpected === sActual)
   console.log('actual>>>', actual);
   console.log('expected>>>', expected);
-})();
+})(2);
+
+
+(function(numberOfDiscs) {
+  console.log('-------------------------------')
+  console.log('numberOfDiscs', numberOfDiscs);
+  const input = [[3, 2, 1],[],[]];
+  const expected = [[], [], [3, 2, 1]];
+  const actual = towersOfHanoi(input);
+  const sInput = JSON.stringify(input);
+  const sExpected = JSON.stringify(expected);
+  const sActual = JSON.stringify(actual);
+  console.log(`${sInput}>>>`, sExpected === sActual)
+  console.log('actual>>>', actual);
+  console.log('expected>>>', expected);
+})(3);
