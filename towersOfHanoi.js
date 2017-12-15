7bb1f0e60c1e0d479269782978d871608b90d5fe
d-kang <david.kang714@gmail.com>
2017-12-15 10:13:51 -0800

    Add While, solves for 5

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -218,31 +218,17 @@ const move3Discs = function(start, temp, end) {
 }
 
 const towers = function(n) {
-  const A = new Tower(n); //[3, 2, 1]
+  const A = new Tower(n);
   const B = new Tower();
   const C = new Tower();
 
-  // moveDonuts(A, B, C);
   return [A, B, C];
-
-  // find which tower is the start - use peek();
-  // which is the end?
-  // the one with the next smallest is the end,
-  // but if they are both empty then C is end by default
-
-  // check if tower A is either odd - use size();
-    // moveDonuts()
-
-  // check if tower A is either even - use size();
-    // moveDonuts()
 };
 
 
 const solver = function(n) {
   const hanoi = towers(n);
   const [ A, B, C ] = hanoi;
-  // console.log('hanoi', hanoi);
-  // console.log([A.stack, B.stack, C.stack])
   let [start, temp, end] = hanoi
 
   if (n % 2 === 0) {
@@ -252,47 +238,35 @@ const solver = function(n) {
 
   move3Discs(start, temp, end);
   moveDonut(start, temp)
-  // [ [ 5 ], [ 4 ], [ 3, 2, 1 ] ]
   let tempStart, tempTemp, tempEnd;
 
-  tempStart = start
-  tempTemp = temp
-  tempEnd = end
-  start = tempEnd;
-  temp = tempStart;
-  end = tempTemp;
-  move3Discs(start, temp, end);
 
-  moveDonut(temp, start)
-
-  tempStart = start
-  tempTemp = temp
-  tempEnd = end
-  start = tempEnd
-  temp = tempStart
-  end = tempTemp
-  move3Discs(start, temp, end);
-  // the one move disc will go from start to temp
-  moveDonut(start, temp)
-
-  tempStart = start
-  tempTemp = temp
-  tempEnd = end
-  start = tempEnd
-  temp = tempStart
-  end = tempTemp
-  move3Discs(start, temp, end);
-
-
-
-  moveDonut(start, temp)
-  const stopTime = Date.now() + 5000;
+  const stopTime = Date.now() + 10000;
   while(Date.now() < stopTime) {
-
+    tempStart = start
+    tempTemp = temp
+    tempEnd = end
+    start = tempEnd;
+    temp = tempStart;
+    end = tempTemp;
+    move3Discs(start, temp, end);
     if (A.stack.length === 0 && B.stack.length === 0) {
       return [A.stack, B.stack, C.stack]
     }
+    moveDonut(temp, start)
 
+    tempStart = start
+    tempTemp = temp
+    tempEnd = end
+    start = tempEnd
+    temp = tempStart
+    end = tempTemp
+    move3Discs(start, temp, end);
+    if (A.stack.length === 0 && B.stack.length === 0) {
+      return [A.stack, B.stack, C.stack]
+    }
+    // the one move disc will go from start to temp
+    moveDonut(start, temp)
   }
 
   return [A.stack, B.stack, C.stack]
@@ -300,184 +274,7 @@ const solver = function(n) {
 
 // by looking at all three know what move needs to made that round and recurse
 // repeatable pattern between odds .... and between evens?
-const moveDonuts = function(start, temp, end) {
-  // do the thing
-  // n = 1
-  // moveDonut(start, end)
 
-  // n = 2
-  // moveDonut(start, temp);
-  // moveDonut(start, end);
-  // moveDonut(temp, end);
-
-  // n = 3
-  // moveDonut(start, end);
-  // moveDonut(start, temp);
-  // moveDonut(end, temp);
-  // moveDonut(start, end);
-  // moveDonut(temp, start);
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-
-  // n = 4
-  // moveDonut(start, temp);
-  // moveDonut(start, end);
-  // moveDonut(temp, end);
-  // moveDonut(start, temp);
-  // moveDonut(temp, start);
-  // moveDonut(start, temp);
-  // moveDonut(end, start);
-  // moveDonut(end, temp);
-  // moveDonut(start, temp);
-  // moveDonut(start, end);
-  // // move 3 discs from temp to end
-  // move3Discs(temp, start, end);
-  // alt 4 discs
-  // move3Discs(start, end, temp)
-  // moveDonut(start, end)
-  // move3Discs(temp, start, end)
-
-  // n = 5
-  // moveDonut(start, end);
-  // moveDonut(start, temp);
-  // moveDonut(end, temp);
-  // moveDonut(start, end);
-  // moveDonut(temp, start);
-  // moveDonut(temp, end);
-  // moveDonut(start, end);
-  // // disc 4 placed on temp
-  // moveDonut(start, temp) // last move or a first move
-  // moveDonut(end, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, start)
-  // moveDonut(end, temp)
-  // moveDonut(start, end)
-  // moveDonut(start, temp)
-  // moveDonut(end, temp)
-  // // disc 5 placed on end
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(end, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, start)
-  // // place disc 4 from temp to end
-  // moveDonut(temp, end)
-  // moveDonut(start, end) // 3 disc from start to end
-  // moveDonut(start, temp)
-  // moveDonut(end, temp)
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-
-  // // alt n = 5
-  // move3Discs(start, temp, end) // move 3 discs to end
-  // moveDisc(start, temp) // place next largest disc (4) at temp
-  // move3Discs(end, start, temp) // move 3 discs to temp
-  // moveDisc(start, end) // place next largest disc (5) at end
-  // move3Discs(temp, end, start) // move 3 discs to start
-  // moveDisc(temp, end) // move disc (4) to end
-  // move3Discs(start, temp, end)
-
-
-
-  // // n = 6
-  // moveDonut(start, temp)
-  // moveDonut(start, end)
-  // moveDonut(temp, end)
-  // moveDonut(start, temp)
-  // moveDonut(end, start)
-  // moveDonut(end, temp)
-  // moveDonut(start, temp)
-  // //move (4)
-  // moveDonut(start, end)
-  // // repeat move three
-  // moveDonut(temp, end)
-  // moveDonut(temp, start)
-  // moveDonut(end, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, temp)
-  // moveDonut(start, end)
-  // moveDonut(temp, end)
-  // //move (5)
-  // moveDonut(start, temp)
-  // // repeat move four
-  // /*ATTEMPT TO SWITCH VARIABLEs*/
-  // //Start > temp > end
-  // //End > start > temp
-  // moveDonut(end, start)
-  // moveDonut(end, temp)
-  // moveDonut(start, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, end)
-  // moveDonut(temp, start)
-  // moveDonut(end, start)
-  // //move (4)
-  // moveDonut(end, temp)
-  // // repeat move three
-  // moveDonut(start, temp)
-  // moveDonut(start, end)
-  // moveDonut(temp, end)
-  // moveDonut(start, temp)
-  // moveDonut(end, start)
-  // moveDonut(end, temp)
-  // moveDonut(start, temp)
-  // // Move (6) *************
-  // moveDonut(start, end)
-  // //start > temp > end
-  // //temp > start > end
-  // moveDonut(temp, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(end, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, start)
-  // //move (4)
-  // moveDonut(temp, end)
-  // // repeat move three
-  // moveDonut(start, end)
-  // moveDonut(start, temp)
-  // moveDonut(end, temp)
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-  // //move (5
-  // moveDonut(temp, temp)
-  // // repeat move four
-  // //ATTEMPT TO SWITCH VARIABLEs
-  // //Start > temp > end
-  // //End > start > temp
-  // moveDonut(end, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, start)
-  // moveDonut(end, temp)
-  // moveDonut(start, end)
-  // moveDonut(start, temp)
-  // moveDonut(end, temp)
-  // //move (4)
-  // moveDonut(end, start)
-  // // repeat move three
-  // moveDonut(temp, start)
-  // moveDonut(temp, end)
-  // moveDonut(start, end)
-  // moveDonut(temp, start)
-  // moveDonut(end, temp)
-  // moveDonut(end, start)
-  // moveDonut(temp, start)
-  //
-  // moveDonut(temp, end)
-  //
-  // move3Discs(start, end, temp)
-  // moveDonut(start, end)
-  // move3Discs(temp, start, end)
-
-
-};
 
 
 
@@ -522,16 +319,22 @@ const moveDonuts = function(start, temp, end) {
 //   console.log(`${n} towers`, hanoi);
 //   console.log(JSON.stringify(hanoi) === '[[],[],[6,5,4,3,2,1]]');
 // })(6);
+// (function(n) {
+//   console.log('============================================')
+//   console.log('============================================')
+//   const hanoi = solver(n);
+//   console.log(`${n} towers`, hanoi);
+//   console.log(JSON.stringify(hanoi) === '[[],[],[5,4,3,2,1]]');
+// })(5);
+// (function(n) {
+//   console.log('============================================')
+//   const hanoi = solver(n);
+//   console.log(`${n} towers`, hanoi);
+//   console.log(JSON.stringify(hanoi) === '[[],[],[6,5,4,3,2,1]]');
+// })(6);
 (function(n) {
-  console.log('============================================')
   console.log('============================================')
   const hanoi = solver(n);
   console.log(`${n} towers`, hanoi);
-  console.log(JSON.stringify(hanoi) === '[[],[],[5,4,3,2,1]]');
-})(5);
-(function(n) {
-  console.log('============================================')
-  const hanoi = solver(n);
-  console.log(`${n} towers`, hanoi);
-  console.log(JSON.stringify(hanoi) === '[[],[],[6,5,4,3,2,1]]');
-})(6);
+  console.log(JSON.stringify(hanoi) === '[[],[],[7, 6,5,4,3,2,1]]');
+})(7);
