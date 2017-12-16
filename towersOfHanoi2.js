503937c63f4d0825af3d1d720aef84ed7e08d4e0
d-kang <david.kang714@gmail.com>
2017-12-16 11:19:32 -0800

    Solve 7-9

--- towersOfHanoi2.js
+++ towersOfHanoi2.js
@@ -29,6 +29,27 @@ const arrange5 = function(A, B, C) {
   moveDisc(A, C)
   arrange4(B, A, C)
 }
+const arrange6 = function(A, B, C) {
+  arrange5(A, C, B)
+  moveDisc(A, C)
+  arrange5(B, A, C)
+}
+const arrange7 = function(A, B, C) {
+  arrange6(A, C, B)
+  moveDisc(A, C)
+  arrange6(B, A, C)
+}
+const arrange8 = function(A, B, C) {
+  arrange7(A, C, B)
+  moveDisc(A, C)
+  arrange7(B, A, C)
+}
+const arrange9 = function(A, B, C) {
+  arrange8(A, C, B)
+  moveDisc(A, C)
+  arrange8(B, A, C)
+}
+
 
 const towers = function(n, stacks) {
   let [A, B, C] = stacks;
@@ -46,6 +67,14 @@ const towers = function(n, stacks) {
     arrange4(A, B, C)
   } else if (n === 5) {
     arrange5(A, B, C)
+  } else if (n === 6) {
+    arrange6(A, B, C)
+  } else if (n === 7) {
+    arrange7(...stacks)
+  } else if (n === 8) {
+    arrange8(...stacks)
+  } else if (n === 9) {
+    arrange9(...stacks)
   }
 
 
@@ -106,3 +135,35 @@ const towers = function(n, stacks) {
   console.log(`${n} towers`, hanoi);
   console.log(JSON.stringify(hanoi) === '[[],[],[5,4,3,2,1]]');
 })(5);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[6,5,4,3,2,1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[6,5,4,3,2,1]]');
+})(6);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[7,6,5,4,3,2,1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[7,6,5,4,3,2,1]]');
+})(7);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[8,7,6,5,4,3,2,1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[8,7,6,5,4,3,2,1]]');
+})(8);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[9,8,7,6,5,4,3,2,1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[9,8,7,6,5,4,3,2,1]]');
+})(9);
