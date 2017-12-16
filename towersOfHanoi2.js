291657e32e5664bada091c6deea785bed865a6a3
d-kang <david.kang714@gmail.com>
2017-12-16 11:05:41 -0800

    Solve for 3 discs

--- towersOfHanoi2.js
+++ towersOfHanoi2.js
@@ -14,6 +14,11 @@ const arrange2 = function(stack_A, stack_B, stack_C) {
   moveDisc(stack_A, stack_C)
   moveDisc(stack_B, stack_C)
 }
+const arrange3 = function(stack_A, stack_B, stack_C) {
+  arrange2(stack_A, stack_C, stack_B)
+  moveDisc(stack_A, stack_C)
+  arrange2(stack_B, stack_A, stack_C)
+}
 
 const towers = function(n, stacks) {
   let [A, B, C] = stacks;
@@ -23,9 +28,13 @@ const towers = function(n, stacks) {
   } else if (n === 1) {
     moveDisc(A, C);
     return stacks;
+  } else if (n === 2 ) {
+    arrange2(A, B, C);
+  } else if (n === 3) {
+    arrange3(A, B, C)
   }
 
-  arrange2(A, B, C)
+
 
 
 
@@ -53,8 +62,16 @@ const towers = function(n, stacks) {
 (function(n) {
   console.log('===========================')
   console.log('===========================')
-  const stacks = [[2, 1],[],[]]
+  const stacks = [[2,1],[],[]]
   const hanoi = towers(n, stacks)
   console.log(`${n} towers`, hanoi);
   console.log(JSON.stringify(hanoi) === '[[],[],[2,1]]');
 })(2);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[3,2,1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[3,2,1]]');
+})(3);
