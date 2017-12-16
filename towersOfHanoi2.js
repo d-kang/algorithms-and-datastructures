c052d16d4fb2a8c2e0703b0077c627ba29fcd7d6
d-kang <david.kang714@gmail.com>
2017-12-16 11:03:07 -0800

    Solve for 2

--- towersOfHanoi2.js
+++ towersOfHanoi2.js
@@ -5,13 +5,27 @@
 * @Last modified time: 12.03.2017 07:22pm
 */
 
+const moveDisc = function(stack_A, stack_B) {
+  stack_B.push(stack_A.pop());
+}
+
+const arrange2 = function(stack_A, stack_B, stack_C) {
+  moveDisc(stack_A, stack_B)
+  moveDisc(stack_A, stack_C)
+  moveDisc(stack_B, stack_C)
+}
 
 const towers = function(n, stacks) {
   let [A, B, C] = stacks;
 
   if (n === 0) {
     return stacks
+  } else if (n === 1) {
+    moveDisc(A, C);
+    return stacks;
   }
+
+  arrange2(A, B, C)
 
 
 
@@ -36,3 +50,11 @@ const towers = function(n, stacks) {
   console.log(`${n} towers`, hanoi);
   console.log(JSON.stringify(hanoi) === '[[],[],[1]]');
 })(1);
+(function(n) {
+  console.log('===========================')
+  console.log('===========================')
+  const stacks = [[2, 1],[],[]]
+  const hanoi = towers(n, stacks)
+  console.log(`${n} towers`, hanoi);
+  console.log(JSON.stringify(hanoi) === '[[],[],[2,1]]');
+})(2);
