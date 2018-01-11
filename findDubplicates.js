b6f8e1809bdb7326c309480449dd288c16f05ac2
d-kang <david.kang714@gmail.com>
2018-01-11 12:39:01 -0800

    Find Dups with Edge Case One larger than the other

--- findDubplicates.js
+++ findDubplicates.js
@@ -1,4 +1,34 @@
 function findDuplicates(arr1, arr2) {
+  var min = Math.min(arr1.length, arr2.length);
+  var max = Math.max(arr1.length, arr2.length);
+  if (min ** 2 < max) {
+    console.log('diff lengths ran');
+    return findDuplicatesDifferentLengths(arr1, arr2);
+  } else {
+    console.log('similar lengths ran');
+    return findDuplicatesSimilarLength(arr1, arr2);
+  }
+}
+
+function findDuplicatesDifferentLengths(arr1, arr2) {
+  var bigger = arr1;
+  var smaller = arr2;
+  var duplicates = [];
+  if (bigger.length < smaller.length) {
+    bigger = arr2;
+    smaller = arr1;
+  }
+
+  console.log('binarySearch(arr2, arr1[0])', binarySearch(arr2, arr1[0]));
+  for (var i = 0; i < smaller.length; i++) {
+    if (binarySearch(bigger, smaller[i]) !== -1) {
+      duplicates.push(smaller[i]);
+    }
+  }
+  return duplicates;
+}
+
+function findDuplicatesSimilarLength(arr1, arr2) {
   duplicates = [];
   var i = 0;
   var j = 0;
@@ -16,3 +46,28 @@ function findDuplicates(arr1, arr2) {
   }
   return duplicates
 }
+
+
+function binarySearch(arr, target) {
+  var min = 0;
+  var max = arr.length - 1;
+  var mid;
+
+  while (min <= max) {
+    mid = Math.floor((min + max) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      min = mid + 1;
+    } else {
+      max = mid - 1;
+    }
+  }
+  return - 1;
+}
+
+// similar length
+findDuplicates([1, 2], [1, 2]);
+
+// one much larger than other
+findDuplicates([1, 25], [1, 2, 3, 5, 8, 9, 10, 15, 25, 100]);
